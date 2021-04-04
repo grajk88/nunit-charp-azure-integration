f13f981ad33a8b7c4f6ab2a6b8a53234158a1dfd
grajk88 <vgrk.2017@gmail.com>
2021-04-04 09:10:43 +0100

    Selenium Sample Tests with NUnit.Framework Push

---
+++ SeleniumSample/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SeleniumSample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SeleniumSample")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8986a0bf-763b-483a-8e66-70e7de53143f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SeleniumSample/UnitTest1.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+using Assert = NUnit.Framework.Assert;
+
+namespace SeleniumSample
+{
+    [TestFixture]
+    public class Tests
+    {
+        IWebDriver _webDriver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("headless");
+            _webDriver = new ChromeDriver(chromeOptions);
+        }
+
+        [Test]
+        [Category("smoke")]
+        public void test1()
+        {
+            _webDriver.Navigate().GoToUrl("https://www.google.com");
+            Assert.True(_webDriver.Title.Contains("Google"));
+        }
+
+        [Test]
+        [Category("regression")]
+        public void test2()
+        {
+            _webDriver.Navigate().GoToUrl("https://www.google.com");
+            Assert.True(_webDriver.Title.Contains("Giri"));
+        }
+
+        [Test]
+        [Category("regression")]
+        public void test3()
+        {
+            _webDriver.Navigate().GoToUrl("https://www.google.com");
+            Assert.True(_webDriver.Title.Contains("Google"));
+        }
+        [Test]
+        [Category("regression")]
+        public void test4()
+        {
+            _webDriver.Navigate().GoToUrl("https://www.google.com");
+            Assert.True(_webDriver.Title.Contains("Google"));
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            _webDriver.Close();
+        }
+
+    }
+}
